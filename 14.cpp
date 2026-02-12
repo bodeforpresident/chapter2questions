891b610c470e1ee25f19eeeddce1cbd25e22cdc2
Bode <289588@burlcoschools.org>
2026-02-12 10:17:09 -0500

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 int main() {
-    cout << "Jawhn Dough\n1234 Town Way, Springfield, Wyoming, 04138\n304-344-2213\nOutdoor Activies";
+    cout << "Jawhn Dough\n1234 Town Way, Springfield, Wyoming, 04138\n304-344-2213\nOutdoor Activies"; // Outputs to terminal
 }
