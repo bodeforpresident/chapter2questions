7838b58c8d34482c9af8f011e83aa54965e4ec6d
Bode <289588@burlcoschools.org>
2026-02-12 10:16:05 -0500

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// Global vars
 int mpgTown = 23.5;
 int mpgHigh = 28.9;
 int gasTank = 20;
-int disTown = gasTank*mpgTown;
-int disHigh = gasTank*mpgHigh;
+int disTown = gasTank*mpgTown; // Distance in town equation
+int disHigh = gasTank*mpgHigh; // Distance on the highway equation
 
+// Code
 int main() {
-    cout << "You can travel ";
-    cout << disTown;
-    cout << " miles in town.\n";
-    cout << "You can travel "; 
-    cout << disHigh; 
-    cout << " miles on the highway.";
+    cout << "You can travel " << disTown << " miles in town.\n"; // Outputs the travel distance in town
+    cout << "You can travel " << disHigh << " miles on the highway"; // Outputs the travel distance on the highway
 }
